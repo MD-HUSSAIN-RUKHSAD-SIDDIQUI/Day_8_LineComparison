e1b290de97144cb6d42f48cc802b127418315f2f
MD HUSSAIN <mdhussainrukhsadsiddiqui@gmail.com>
2023-05-07 21:56:03 +0530

    First commit

---
+++ LineComparisonProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Program of Line Comparison Problem:");
+            
+            Console.ReadKey();
+        }
+    }
+}
